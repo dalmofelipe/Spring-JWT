b880e55f80a32b4887fb1304bc4f34010f48b60e
Dalmo Felipe <dalmo.felipe@gmail.com>
2024-09-01 21:11:08 -0300

    implementa campo para header Authorization no front do Swagger

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-## REST API de Gestão de Usuários, Autenticação e Permissão baseada funções (Roles)
+## Spring JWT
 
-**Spring Boot 3 & Spring Security 6**
+Gestão de Usuários, Autenticação e Permissão
 
-### Suporte
-
-- No momento, registro direto de usuários, sem validação via email
-- Autenticação via token JWT com tempo de expiração. Default para teste e de 5min
-- Protenção de rotas baseado ROLES
-- Somente __Administradores__ ou perfil __SET_ROLE__ podem criar novas ROLES
-
-Endpoint | Nível de Acesso
---------- | ------
-error | Rota liberada
-/admin | Somente administrador
-/roles/** | Somente administrador
-/users | Rota liberada
-/users/{id}/role | Somente ADMIN e SET_ROLE
-/auth/** | Rota liberada
-/h2/** | Rota liberada
+Spring Boot 3 & Spring Security 6
--- src/main/java/com/dalmofelipe/SpringJWT/Config/SwaggerConfig.java
+++ src/main/java/com/dalmofelipe/SpringJWT/Config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package com.dalmofelipe.SpringJWT.Config;
 
-
-
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,21 +17,28 @@ public class SwaggerConfig {
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("public-api")
-                .pathsToMatch("/**")
-                .build();
+            .group("public-api")
+            .pathsToMatch("/**")
+            .build();
     }
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Título da API")
-                        .version("1.0")
-                        .description("Descrição da API")
-                        .contact(new Contact()
-                                .name("Dalmo Felipe")
-                                .url("http://seusite.com")
-                                .email("seuemail@dominio.com")));
+            .info(new Info()
+                .title("Título da API")
+                .version("1.0")
+                .description("Descrição da API")
+                .contact(new Contact()
+                    .name("Dalmo Felipe")
+                    .url("http://seusite.com")
+                    .email("seuemail@dominio.com")))
+            .addSecurityItem(new SecurityRequirement()
+                .addList("Bearer Token"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("Bearer Token", new SecurityScheme()
+                    .type(Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
     }
 }
